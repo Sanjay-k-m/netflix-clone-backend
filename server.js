e6c3bb25f17c071a0b07034a768c57352d22dab7
Sanjay-k-m <sanjay_k.m@outlook.com>
2024-09-25 10:52:23 +0530

    added protectRoute Middleware

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
 app.use(express.json()); // will allow us to parse req.body
+app.use(cookieParser()); // will allow us to parse cookies
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
